2f5f11b3af7e341cf22614281ed384e560c1ef8c
Roman <61121402+rebrov-rc@users.noreply.github.com>
2023-03-15 22:58:32 +0300

    update TransferController

--- src/main/java/com/bank/moneytransfer/controllers/TransferController.java
+++ src/main/java/com/bank/moneytransfer/controllers/TransferController.java
@@ -1,10 +1,9 @@
 package com.bank.moneytransfer.controllers;
 
-import com.bank.moneytransfer.entities.Transfer;
-import com.bank.moneytransfer.entities.TransferConfirmOperation;
+import com.bank.moneytransfer.entities.TransferRequest;
+import com.bank.moneytransfer.entities.TransferConfirmOperationRequest;
 import com.bank.moneytransfer.entities.TransferMessage;
 import com.bank.moneytransfer.services.TransferService;
-import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
@@ -12,22 +11,22 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/")
 public class TransferController {
 
-    private TransferService service;
+    public TransferService service;
 
+    public TransferController(TransferService service) {
+        this.service = service;
+    }
 
     @PostMapping("transfer")
-    public TransferMessage postTransfer(@Valid @RequestBody Transfer transfer){
-//        TransferMessage message = new TransferMessage();
-//        message.setOperationId("123456");
+    public TransferMessage postTransfer(@RequestBody TransferRequest transfer){
+
         return this.service.postTransfer(transfer);
     }
 
-    @PostMapping("confirmOperation")
-    public TransferMessage postConfirmOperation(@Valid @RequestBody TransferConfirmOperation confirmOperation){
 
-//        System.out.println(confirmOperation.toString());
-//        TransferMessage message = new TransferMessage();
-//        message.setOperationId("12345");
+    @PostMapping("confirmOperation")
+    public TransferMessage postConfirmOperation(@RequestBody TransferConfirmOperationRequest confirmOperation){
+
         return this.service.postConfirmOperation(confirmOperation);
     }
 
